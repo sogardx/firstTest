4fbccbebcb9857872c2825730c29703093091192
Sogard <chiperdragos@gmail.com>
2022-04-18 15:19:15 +0300

    Add project files.

---
+++ firstTest/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+namespace firstTest
+{
+    public class Class1
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://politrip.com/account/sign-up");
+
+                TestHelper.Pause();
+
+                string pageTitle = driver.Title;
+
+                Assert.Equal("Sign up | Politrip", pageTitle);
+                Assert.Equal("https://politrip.com/account/sign-up", driver.Url);
+            }
+
+        }
+    }
+}
---
+++ firstTest/TestHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+
+namespace firstTest
+{
+    internal static class TestHelper
+    {
+        public static void Pause(int secondsToPause = 3000)
+        {
+            Thread.Sleep(secondsToPause);
+        }
+    }
+}
